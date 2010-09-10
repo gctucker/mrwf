f34ed34eb3d407abb8016c170936d559e3693e5b
Guillaume Tucker <guillaume.tucker@laposte.net>
2010-09-10 08:02:04 +0100

    extra.public.apply: send e-mail when an application is received to listeners

--- extra/public/apply.py
+++ extra/public/apply.py
@@ -3,11 +3,12 @@ from django.http import (HttpResponse, HttpResponseServerError,
                          HttpResponseRedirect)
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
 from django.shortcuts import render_to_response, get_object_or_404
 from django.conf import settings
 from cams.models import (Person, Contact, PersonContact, Organisation,
                          Fair, Participant)
-from mrwf.extra.models import StallEvent, StallApplication
+from mrwf.extra.models import StallEvent, StallApplication, Listener
 
 class PersonForm (forms.ModelForm):
     last_name = forms.CharField (max_length = 127, required = True)
@@ -70,7 +71,7 @@ class StallApplicationForm ():
     def is_valid (self):
         return self.p.is_valid () and self.s.is_valid () and self.c.is_valid ()
 
-    def save (self):
+    def save (self, rcpts):
         # save personal details
         self.p.instance.status = 0
         self.p.save ()
@@ -98,7 +99,39 @@ class StallApplicationForm ():
         sa.event = self.s.instance
         sa.save ()
 
-        # ToDo: send e-mail
+        stall = self.s.instance
+        contact = self.c.instance
+
+        if stall.main_contact == None:
+            main_contact = 'none'
+        elif stall.main_contact == 0: # StallEvent.TELEPHONE
+            main_contact = 'telephone'
+        elif stall.main_contact == 1:
+            main_contact = 'email'
+        elif stall.main_contact == 2:
+            main_contact = 'website'
+        else:
+            main_contact = "unknown (%d)" % stall.main_contact
+
+        if rcpts:
+            subject = "Stallholder application - %s" % part.person
+            msg = "Date:         %s\n" % sa.created
+            msg += "Person:       %s\n" % part.person
+            msg += "Line 1:       %s\n" % contact.line_1
+            msg += "Line 2:       %s\n" % contact.line_2
+            msg += "Line 3:       %s\n" % contact.line_3
+            msg += "Town:         %s\n" % contact.town
+            msg += "Postcode:     %s\n" % contact.postcode
+            msg += "E-mail:       %s\n" % contact.email
+            msg += "Website:      %s\n" % contact.website
+            msg += "Telephone:    %s\n" % contact.telephone
+            msg += "Mobile:       %s\n" % contact.mobile
+            msg += "Stall name:   %s\n" % stall.name
+            msg += "Spaces:       %d\n" % stall.n_spaces
+            msg += "Tables:       %d\n" % stall.n_tables
+            msg += "Main contact: %s\n" % main_contact
+            msg += "Description:  %s\n" % stall.description
+            send_mail (subject, msg, "no-reply@mangoz.org", rcpts)
 
 # -----------------------------------------------------------------------------
 
@@ -125,7 +158,22 @@ def post (request):
     if not form.is_valid ():
         return stallholder_form (request, form)
     else:
-        form.save ()
+        rcpts = []
+        trigger = Listener.STALL_APPLICATION_RECEIVED
+        listeners = Listener.objects.filter (trigger = trigger)
+        for l in listeners:
+            u = l.user
+            if u.email:
+                rcpts.append (u.email)
+            else:
+                part = Participant.objects.get (user = u)
+                p = part.person
+                contacts = PersonContact.objects.filter (person = p)
+                for c in contacts:
+                    if c.email:
+                        rcpts.append (c.email)
+                        break
+        form.save (rcpts)
         return HttpResponseRedirect (reverse (application))
 
 
