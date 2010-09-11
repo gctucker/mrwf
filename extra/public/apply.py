5df97ee22da60da3a18b2607c1930a76574aa3c7
Guillaume Tucker <guillaume.tucker@laposte.net>
2010-09-11 10:56:28 +0100

    move some forms from public.apply to forms
    . PersonForm for cams.models.Person
    . PersonContactForm for cams.models.PersonContact
    . StallForm for extra.models.StallEvent

--- extra/public/apply.py
+++ extra/public/apply.py
@@ -6,60 +6,9 @@ from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.shortcuts import render_to_response, get_object_or_404
 from django.conf import settings
-from cams.models import (Person, Contact, PersonContact, Organisation,
-                         Fair, Participant, Application, get_user_email)
+from cams.models import (Fair, Participant, Application, get_user_email)
 from mrwf.extra.models import StallEvent, FairEventApplication, Listener
-
-class PersonForm (forms.ModelForm):
-    last_name = forms.CharField (max_length = 127, required = True)
-
-    class Meta:
-        model = Person
-        exclude = ['nickname', 'alter', 'status']
-
-
-class PersonContactForm (forms.ModelForm):
-    line_1 = forms.CharField (max_length = 63, required = True)
-    town = forms.CharField (max_length = 63, required = True)
-    postcode = forms.CharField (max_length = 15, required = True)
-    email = forms.EmailField (max_length = 127, required = True,
-                              help_text = Contact.email_help_text)
-
-    class Meta:
-        model = PersonContact
-        exclude = ['status', 'person', 'addr_order', 'addr_suborder',
-                   'country', 'fax']
-
-
-def check_ibounds (data, name, imin, imax):
-    try:
-        val = int (data[name])
-
-        if (val < imin) or (val > imax):
-            raise forms.ValidationError (
-                "Value out of bounds (min=%d, max=%d)" % (imin, imax))
-        else:
-            return val
-
-    except ValueError:
-        raise forms.ValidationError ("Invalid value for %s" % name)
-
-
-class StallForm (forms.ModelForm):
-    attrs = {'cols': '60', 'rows': '3'}
-    description = forms.CharField (widget = forms.Textarea (attrs = attrs))
-
-    class Meta:
-        model = StallEvent
-        fields = ('name', 'n_spaces', 'n_tables', 'main_contact',
-                  'description')
-
-    def clean_n_spaces (self):
-        return check_ibounds (self.cleaned_data, 'n_spaces', 1, 3)
-
-    def clean_n_tables (self):
-        return check_ibounds (self.cleaned_data, 'n_tables', 0, 3)
-
+from mrwf.extra.forms import PersonForm, PersonContactForm, StallForm
 
 class StallApplicationForm ():
     def __init__ (self, p = PersonForm (), c = PersonContactForm (),
