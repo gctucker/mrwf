a5e3e4b8386b68f8bb88419ed197d92426c49d0f
Guillaume Tucker <guillaume@mangoz.org>
2011-08-15 21:06:39 +0100

    extra.views.abook: group all search functions in SearchHelper

--- extra/views/abook.py
+++ extra/views/abook.py
@@ -10,72 +10,152 @@ from mrwf.extra.views.main import SiteView, get_list_page
 class SearchHelper(object):
     def __init__(self, request):
         self.form = SearchHelper.SearchForm(request.GET)
+        self._people = []
+        self._orgs = []
         if self.form.is_valid():
             self.match = self.form.cleaned_data['match']
+            self.keywords = str2list(self.match)
             self.opt_contacts = self.form.cleaned_data['opt_contacts']
             self.urlmatch = urlencode((('match', self.match),
                                        ('opt_contacts',
                                         self.opt_contacts)))
+            self._do_search()
         else:
             self.match = ''
-            self.urlmatch = ''
+            self.keywords = []
             self.opt_contacts = False
+            self.urlmatch = ''
+
+    @property
+    def people(self):
+        return self._people
+
+    @property
+    def orgs(self):
+        return self._orgs
+
+    @property
+    def has_results(self):
+        if self._people or self._orgs:
+            return True
+        return False
+
+    def _do_search(self):
+        if self.opt_contacts:
+            self._search_in_contacts()
+        else:
+            self._search_in_names()
+
+    def _search_in_names(self):
+        p_list = list(self._search_people())[:30]
+        o_list = list(self._search_orgs())[:30]
+
+        for p in p_list:
+            c = p.contact_set.all()
+            if c.count() == 0:
+                m = Member.objects.filter(person = p)
+                if m.count() > 0:
+                    c = m[0].contact_set.all()
+            self._append_person(p, c)
+
+        for o in o_list:
+            c = o.contact_set.all()
+            if c.count() == 0:
+                m = Member.objects.filter(organisation = o)
+                if m.count() > 0:
+                    c = m[0].contact_set.all()
+            self._append_org(o, c)
+
+    def _search_in_contacts(self):
+        # Note: There may be several matching contacts related to the same
+        # person/org, so that person/org will be in the results several
+        # times (with a different contact preview though)
+        contacts = list(self._search_contacts())[:40]
+
+        for c in contacts:
+            obj = Contactable.objects.get(pk=c.obj_id)
+            # ToDo: transform (cast) a Contactable to its subclass...
+            if obj.type == Contactable.ORGANISATION:
+                # ToDo: check whether that reads the Contactable data again
+                self._append_org(obj.organisation, (c,))
+            elif obj.type == Contactable.PERSON:
+                self._append_person(obj.person, (c,))
+            elif obj.type == Contactable.MEMBER:
+                self._append_person(obj.member.person, (c,))
+            else:
+                pass # ToDo: throw exception
+
+    def _append_person(self, p, c):
+        self._people.append({'first_name': p.first_name,
+                             'middle_name': p.middle_name,
+                             'last_name': p.last_name,
+                             'nickname': p.nickname,
+                             'id': p.id,
+                             'contacts': c})
+
+    def _append_org(self, o, c):
+        self._orgs.append({'name': o.name,
+                           'nickname': o.nickname,
+                           'id': o.id,
+                           'contacts': c})
+
+    def _search_people(self):
+        people = Person.objects.all()
+
+        for kw in self.keywords:
+            people = people.filter(Q(first_name__icontains=kw) |
+                                   Q(middle_name__icontains=kw) |
+                                   Q(last_name__icontains=kw) |
+                                   Q(nickname__icontains=kw))
+
+        return people.filter(status=Record.ACTIVE)
+
+    def _search_orgs(self):
+        orgs = Organisation.objects.all()
+
+        for kw in self.keywords:
+            orgs = orgs.filter(Q(name__icontains=kw) |
+                               Q(nickname__icontains=kw))
+
+        return orgs.filter(status=Record.ACTIVE)
+
+    def _search_contacts(self):
+        contacts = Contact.objects.all()
+
+        tel_re = None
+        for c in self.match.strip():
+            if c.isdigit():
+                if tel_re:
+                    tel_re += r'[^0-9]*' + c
+                elif c != '0':
+                    tel_re = c
+
+        if not tel_re:
+            tel_re = r'^\[\]$'
+
+        for kw in self.keywords:
+            # ToDo: find a way to avoid testing the same tel_re with each kw
+            contacts = contacts.filter(Q(line_1__icontains=kw) |
+                                       Q(line_2__icontains=kw) |
+                                       Q(line_3__icontains=kw) |
+                                       Q(town__icontains=kw) |
+                                       Q(postcode__icontains=kw) |
+                                       Q(country__icontains=kw) |
+                                       Q(email__icontains=kw) |
+                                       Q(website__icontains=kw) |
+                                       Q(telephone__regex=tel_re) |
+                                       Q(mobile__regex=tel_re) |
+                                       Q(fax__regex=tel_re) |
+                                       Q(addr_order__icontains=kw) |
+                                       Q(addr_suborder__icontains=kw))
+
+        return contacts.filter(status=Record.ACTIVE)
 
     class SearchForm(forms.Form):
         match = forms.CharField(required=True, max_length=64)
         opt_contacts = forms.BooleanField(required = False,
                                           label = "search within contacts")
 
-
-def search_people(keywords):
-    people = Person.objects.all()
-
-    for kw in keywords:
-        people = people.filter(Q(first_name__icontains=kw) |
-                               Q(middle_name__icontains=kw) |
-                               Q(last_name__icontains=kw) |
-                               Q(nickname__icontains=kw))
-
-    return people.filter(status=Record.ACTIVE)
-
-def search_orgs(keywords):
-    orgs = Organisation.objects.all()
-
-    for kw in keywords:
-        orgs = orgs.filter(Q(name__icontains=kw) |
-                           Q(nickname__icontains=kw))
-
-    return orgs.filter(status=Record.ACTIVE)
-
-def search_contacts(keywords):
-    contacts = Contact.objects.all()
-
-    tel_re = r''
-    for kw in keywords:
-        for c in kw:
-            if c.isdigit():
-                if tel_re:
-                    tel_re += r'[^0-9]*' + c
-                elif c != '0':
-                    tel_re += c
-
-    for kw in keywords:
-        # ToDo: find a way to avoid testing the same tel_re with each kw
-        contacts = contacts.filter(Q(line_1__icontains=kw) |
-                                   Q(line_2__icontains=kw) |
-                                   Q(line_3__icontains=kw) |
-                                   Q(town__icontains=kw) |
-                                   Q(postcode__icontains=kw) |
-                                   Q(country__icontains=kw) |
-                                   Q(email__icontains=kw) |
-                                   Q(website__icontains=kw) |
-                                   Q(telephone__regex=tel_re) |
-                                   Q(mobile__regex=tel_re) |
-                                   Q(fax__regex=tel_re) |
-                                   Q(addr_order__icontains=kw) |
-                                   Q(addr_suborder__icontains=kw))
-
-    return contacts.filter(status=Record.ACTIVE)
 
 class AbookView(SiteView):
     title = 'Address Book'
@@ -109,82 +189,18 @@ class SearchView(AbookView):
 
     def get_context_data(self, **kwargs):
         ctx = super(SearchView, self).get_context_data(**kwargs)
-        self.search = SearchHelper(self.request)
+        search = SearchHelper(self.request)
 
-        if self.search.match:
-            self.set_search_results()
+        if search.has_results:
+            results = SearchView.PaginatorStub()
+            results.add_list(search.people, 'person')
+            results.add_list(search.orgs, 'org')
+            results.limit_list(40)
         else:
-            self.results = None
+            results = None
 
-        ctx.update({'form': self.search.form, 'page': self.results})
+        ctx.update({'form': search.form, 'page': results})
         return ctx
-
-    def set_search_results(self):
-        keywords = str2list(self.search.match)
-        self._people = []
-        self._orgs = []
-
-        if self.search.opt_contacts:
-            self._search_in_contacts(keywords)
-        else:
-            self._search_in_names(keywords)
-
-        self.results = SearchView.PaginatorStub()
-        self.results.add_list(self._people, 'person')
-        self.results.add_list(self._orgs, 'org')
-        self.results.limit_list(40)
-
-    def _search_in_names(self, keywords):
-        p_list = list(search_people(keywords))[:30]
-        o_list = list(search_orgs(keywords))[:30]
-
-        for p in p_list:
-            c = p.contact_set.all()
-            if c.count() == 0:
-                m = Member.objects.filter(person = p)
-                if m.count() > 0:
-                    c = m[0].contact_set.all()
-            self._append_person(p, c)
-
-        for o in o_list:
-            c = o.contact_set.all()
-            if c.count() == 0:
-                m = Member.objects.filter(organisation = o)
-                if m.count() > 0:
-                    c = m[0].contact_set.all()
-            self._append_org(o, c)
-
-    def _search_in_contacts(self, keywords):
-        # Note: There may be several matching contacts related to the same
-        # person/org, so that person/org will be in the results several
-        # times (with a different contact preview though)
-        contacts = list(search_contacts(keywords))[:40]
-        for c in contacts:
-            obj = Contactable.objects.get(pk=c.obj_id)
-            # ToDo: transform (cast) a Contactable to its subclass...
-            if obj.type == Contactable.ORGANISATION:
-                # ToDo: check whether that reads the Contactable data again
-                self._append_org(obj.organisation, (c,))
-            elif obj.type == Contactable.PERSON:
-                self._append_person(obj.person, (c,))
-            elif obj.type == Contactable.MEMBER:
-                self._append_person(obj.member.person, (c,))
-            else:
-                pass # ToDo: throw exception
-
-    def _append_person(self, p, c):
-        self._people.append({'first_name': p.first_name,
-                             'middle_name': p.middle_name,
-                             'last_name': p.last_name,
-                             'nickname': p.nickname,
-                             'id': p.id,
-                             'contacts': c})
-
-    def _append_org(self, o, c):
-        self._orgs.append({'name': o.name,
-                           'nickname': o.nickname,
-                           'id': o.id,
-                           'contacts': c})
 
     class PaginatorStub(object):
         def __init__(self):
