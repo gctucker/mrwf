0abe528fe2e569e8b59e170b4d7558d8b9a607e7
Guillaume Tucker <guillaume@mangoz.org>
2013-03-27 23:49:14 +0000

    local_settings_sample.py: update with best default values
    When using SQLite, users in new repositories should only
    need to adjust the BASE_DIR value to the path where they
    cloned the mrwf and cams repositories (as explained in
    README.rst).

--- local_settings_sample.py
+++ local_settings_sample.py
@@ -1,27 +1,27 @@
+import os
 import settings
+
+BASE_DIR = '/home/user/path/to/django/mrwf'
 
 settings.DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
-        'USER': 'dbuser',
-        'PASSWORD': 'pwd',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'mrwf.db'),
     }
 }
 
-#settings.MEDIA_ROOT = 
-#settings.MEDIA_URL = 
-#settings.ADMIN_MEDIA_PREFIX = 
-#settings.TEMPLATE_DIRS += ('/path/',)
-#settings.STATIC_ROOT = 
+settings.LOG_PATH = 'log'
+settings.MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
+settings.MEDIA_URL = '/static/mrwf/upload/'
+settings.ADMIN_MEDIA_PREFIX = '/media/'
+settings.TEMPLATE_DIRS += (os.path.join(BASE_DIR, 'templates'),)
+settings.STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+settings.URL_PREFIX = '/'
+settings.LOGIN_URL = settings.URL_PREFIX + 'accounts/login'
+settings.LOGIN_REDIRECT_URL = settings.URL_PREFIX
+
 #settings.SECRET_KEY = 
 #settings.ADMINS += (('Name', 'name@email.com'), )
-
-#settings.URL_PREFIX = '/'
-#settings.LOGIN_URL = settings.URL_PREFIX + 'accounts/login'
-#settings.LOGIN_REDIRECT_URL = settings.URL_PREFIX
 
 #settings.EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #settings.EMAIL_FILE_PATH =
