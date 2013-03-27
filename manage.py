560f416fe162fa966be39d9aadcde5c394d7420e
Guillaume Tucker <guillaume@mangoz.org>
2013-03-27 23:48:12 +0000

    update `manage.py' to latest version for development

--- manage.py
+++ manage.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-
 import os
-os.environ['SITE_CONF'] = 'site.conf'
-
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mrwf.settings")
+
+    from django.core.management import execute_from_command_line
+    sys.path.append(os.path.split(os.getcwd())[0])
+    import settings
+
+    execute_from_command_line(sys.argv)
